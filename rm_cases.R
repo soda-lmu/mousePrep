db21735598cb341a1e89cbe4c37e725a86cc2341
Aida Kreider <aida.kreider99@gmail.com>
2025-09-10 15:58:07 +0200

    Add Description and function

---
+++ rm_cases.R
@@ -0,0 +1,41 @@
+#' Remove cases from dataset 
+#'
+#' This function takes the data and the specified participant ID, session ID, the selected column,
+#' which entails the filter variable, the filter variable, the filter criteria and returns the filtered 
+#' dataframe. 
+#' 
+#' Can be used to remove cases where the window was resized or to remove participants who used a touch device.
+#'
+#' @param data A dataframe with the mouse movement data. 
+#' @param part_id The participant ID.
+#' @param sess_id The session or worker ID, identifies the seperated session, one participant has multiple IDs. 
+#' @param column_rm The column, which is used for the filter selection.
+#' @param factor_rm The factor / category which should be removed. 
+#' @param criteria For 1: Remove only the session / worker Id, where the criteria are met.
+#'                 For 2: Remove all data from the participant, where the criteria are met. 
+#' @return A filtered dataframe with removed cases. 
+#' @examples
+#' rm_cases(data, factor_rm = "resize", criteria = 1)
+#' rm_cases(data, column_rm = "userAgent_is_touch_capable", factor_rm = TRUE)
+#' @export
+
+
+
+rm_cases <- function(data, part_id = "mouseid", sess_id = "workerId", column_rm = "type", factor_rm, criteria) {
+  
+  if (criteria == "1"){
+    
+    resize_mt_ids <- unique(data[[sess_id]][data[[column_rm]] == factor_rm])  
+    new_data <- data[which(!(data[[sess_id]] %in% resize_mt_ids)),]
+    
+    return(resize_mt_ids)
+    
+  } else if (criteria == "2"){
+    
+    resize_mt_ids <- unique(data[[part_id]][data[[column_rm]] == factor_rm])  
+    new_data <- data[which(!(data[[part_id]] %in% resize_mt_ids)),]
+    
+    return(resize_mt_ids)  
+    
+  } 
+}

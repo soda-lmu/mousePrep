117b5cffb9ef2b0cb857aa7298aed9e9b894ba4f
Aida Kreider <aida.kreider99@gmail.com>
2025-09-18 10:58:51 +0200

    ideas for new fcts

---
+++ R/pre_mousetrap.R
@@ -0,0 +1,20 @@
+#' Prepares data to load error-free into the mousetrap package. 
+#'
+#' This function ...
+#' --> Keep the timestamp in right order 
+#' --> remove cases with less than three cases per screen
+#'
+#'
+#' @param data A data frame with the mouse movement data. 
+#' @param 
+#' 
+#' @return 
+#' @examples
+#'
+#' @export
+
+pre_mousetrap <- function(data) {
+  
+  
+  
+}
---
+++ R/rm_cases_time.R
@@ -0,0 +1,20 @@
+#' Remove cases from data set based on time spent on one question
+#'
+#' This function ...
+#' 
+#' Can be used to remove cases where e.g.:
+#'  - time spent on a questions is not within 2 SD of whole data set. 
+#'
+#' @param data A data frame with the mouse movement data. 
+#' @param 
+#' 
+#' @return 
+#' @examples
+#'
+#' @export
+
+rm_cases_time <- function(data) {
+  
+  
+
+}
